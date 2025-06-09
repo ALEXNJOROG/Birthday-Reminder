8e5cb22ef28283ee7697a94e64f639f9300c5373
ALEXNJOROG <alex.njoroge@student.moringaschool.com>
2025-06-09 14:48:30 +0300

    Third Commit

--- birthday-dashboard/src/data/sampleData.js
+++ birthday-dashboard/src/data/sampleData.js
@@ -1,28 +1,7 @@
 export const sampleBirthdays = [
-  {
-    id: 1,
-    name: "Sarah Johnson",
-    date: "2024-12-15",
-    relationship: "Best Friend",
-    age: 25,
-    color: "bg-pink-500"
-  },
-  {
-    id: 2,
-    name: "Mom",
-    date: "2024-08-22",
-    relationship: "Family",
-    age: 52,
-    color: "bg-purple-500"
-  },
-  {
-    id: 3,
-    name: "Alex Chen",
-    date: "2024-07-08",
-    relationship: "Colleague",
-    age: 28,
-    color: "bg-blue-500"
-  }
+ 
+  
+  
 ];
 
 export const colors = [
